7768f1bb4e9268104cc608ee9fc37f6331beb5e3
闫秋阳 <yanjiuyangok@163.com>
2019-03-18 18:27:01 +0800

    databinding基本使用

---
+++ app/src/main/java/com/yqy/jetpack2/Bean.kt
@@ -0,0 +1,11 @@
+package com.yqy.jetpack2
+
+/**
+ * @desc
+ * @author derekyan
+ * @date 2019/3/18
+ */
+class Bean {
+}
+
+data class User(val firstName: String, val lastName: String, val age: Byte)
--- app/src/main/java/com/yqy/jetpack2/MainActivity.kt
+++ app/src/main/java/com/yqy/jetpack2/MainActivity.kt
@@ -2,11 +2,16 @@ package com.yqy.jetpack2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.yqy.jetpack2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.user = User("Test", "User", 11)
     }
 }
